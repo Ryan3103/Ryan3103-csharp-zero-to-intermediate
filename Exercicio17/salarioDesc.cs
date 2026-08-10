6b2561be1f371b54e024e5fa28c9bb1be1476b93
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-10 15:44:46 -0300

    mensagem descrevendo o que voce fez

---
+++ Exercicio17/salarioDesc.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Digite seu nome: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Digite seu salário: ");
+double salario = double.Parse(Console.ReadLine());
+
+double bonus;
+double salarioFinal;
+
+if (salario <= 2000)
+bonus = 0.10;
+else
+bonus = 0.05;
+
+```
+salarioFinal = salario + bonus;
+
+Console.WriteLine("Salario: " + salario);
+ Console.WriteLine("Bonus: " + bonus);
+  Console.WriteLine("Salario final: " + salarioFinal);
