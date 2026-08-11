6150fd295e3caa660f1505e6c97110b452e00e11
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-11 15:22:04 -0300

    Novas atts

---
+++ Exercicio21/While2.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite uma senha: ");
+int senha = int.Parse(Console.ReadLine());
+
+while (senha != 1234) {
+    Console.WriteLine("Senha incorreta. Digite novamente:");
+    senha = int.Parse(Console.ReadLine()); 
+    }
+
+    Console.WriteLine("Acesso permitido!");
