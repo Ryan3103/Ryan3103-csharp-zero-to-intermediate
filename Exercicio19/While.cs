bfe5437fa224c86764772dad58a5bb8b5f2510ea
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-11 14:30:28 -0300

    novas atividades

---
+++ Exercicio19/While.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+while (numero >= 1)
+{
+    Console.WriteLine(numero);
+    numero--;
+}
---
+++ Exercicio20/While1.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+while (numero != 0)
+{
+    Console.WriteLine("Digite um número: ");
+    numero = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Progrma encerrado!");
