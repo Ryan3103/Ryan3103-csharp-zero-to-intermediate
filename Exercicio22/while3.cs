bc96dd88bed8cd50154d2a72f3946e9f21ad418b
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-12 14:41:53 -0300

    mais pastas

---
+++ Exercicio22/while3.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Digite um numero: ");
+int numero = int.Parse(Console.ReadLine());
+
+double soma = 0;
+int contador = 0;
+double media = 0;
+
+
+while (numero != 0) {
+    contador++;
+    soma = soma + numero;
+    numero = int.Parse(Console.ReadLine());
+}
+
+media = soma / contador;
+
+Console.WriteLine("Quantidade: " + contador);
+Console.WriteLine("Soma: " + soma);
+Console.WriteLine("media: " + media);
