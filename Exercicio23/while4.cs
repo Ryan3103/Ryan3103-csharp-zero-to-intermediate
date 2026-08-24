e28598a008f0c9ed2613015a4fdebffa43c4c02a
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-24 16:06:36 -0300

    c#

--- Exercicio23/while4.cs
+++ Exercicio23/while4.cs
@@ -1,6 +1,29 @@
-﻿int contador = 10;
+﻿Console.WriteLine("Digite um número: ");
+int n1 = int.Parse(Console.ReadLine());
 
-while (contador > 0) {
-    contador--;
-    Console.WriteLine(contador);
+Console.WriteLine("Digite outro número: ");
+int n2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a operação: ( +, -, *, /)");
+string operacao = Console.ReadLine();
+
+int resultado;
+
+if (operacao == "+") {
+    resultado = n1 + n2;
 }
+else if (operacao == "-") {
+    resultado = n1 - n2;
+}
+else if (operacao == "*") {
+    resultado = n1 * n2;
+}
+else if (operacao == "/") {
+    resultado = n1 / n2;
+}
+else {
+    Console.WriteLine("Operação inválida!");
+    return;
+}
+
+Console.WriteLine($"resultado: {resultado}");
