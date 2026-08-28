ae5b6116eea2025cc1221c6cbc497b8e99e08a6f
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-28 19:53:04 -0300

    somaFor

---
+++ Exercicio39/somaFor.cs
@@ -0,0 +1,11 @@
+﻿
+int soma = 0;
+
+for (int i = 1; i <= 10; i++) {
+    Console.WriteLine("Digite um numero: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    soma = soma + numero;
+    }
+    
+    Console.WriteLine(soma);
