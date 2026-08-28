6ede18e35775c06ae8fa865998232127b6e8776b
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-28 19:34:10 -0300

    multiplicacao

---
+++ Exercicio38/multiplicaçao.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Digite um numero que deseje multiplicar: ");
+int numero = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 10; i++)
+    Console.WriteLine($"A multipicação de {numero} por {i} é: {numero * i}");
