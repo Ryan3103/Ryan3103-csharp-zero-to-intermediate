9352d426a87847e3359098439104b124c4e795f0
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-29 14:39:14 -0300

    switch

---
+++ Switchs/Switch01/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Digite 1, 2 ou 3!");
+int numero = int.Parse(Console.ReadLine());
+
+switch (numero)
+{
+    case 1:
+    Console.WriteLine("Digitou 1!");
+    break;
+
+    case 2: 
+    Console.WriteLine("Digitou 2!");
+    break;
+
+    case 3:
+    Console.WriteLine("Digitou 3!");
+    break;
+
+    default:
+    Console.WriteLine("Opção invalida!");
+    break;
+    }
