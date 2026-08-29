3a5d636c638e4b84619f7e83f719715d66ae6a89
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-29 14:47:02 -0300

    atv2

---
+++ Switchs/Switch02/opt1.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("1 - Cadastrar");
+Console.WriteLine("2 - Consultar");
+Console.WriteLine("3 - Editar");
+Console.WriteLine("4 - Sair");
+int opcao = int.Parse(Console.ReadLine());
+
+switch (opcao)
+{
+    case 1:
+    Console.WriteLine("Cadastrar");
+    break;
+    case 2:
+    Console.WriteLine("Consultar");
+    break;
+    case 3:
+    Console.WriteLine("Editar");
+    break;
+    case 4:
+    Console.WriteLine("Sair");
+    break;
+    default:
+    Console.WriteLine("Opção invalida!");
+    break;
+}
