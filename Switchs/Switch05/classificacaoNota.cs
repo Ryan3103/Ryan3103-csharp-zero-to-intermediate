5a4a466330b493c37360f38a606a51985ae944fe
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-30 12:18:11 -0300

    caixa2

---
+++ Switchs/Switch05/classificacaoNota.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Digite uma nota: (0-5)");
+int nota = int.Parse(Console.ReadLine());
+
+switch (nota)
+{
+    case 1:
+    Console.WriteLine("Péssimo");
+    break;
+
+    case 2:
+    Console.WriteLine("Ruim");
+    break;
+
+    case 3:
+    Console.WriteLine("Regular");
+    break;
+
+    case 4:
+    Console.WriteLine("Bom");
+    break;
+
+    case 5:
+    Console.WriteLine("Excelente");
+    break;
+
+    default:
+    Console.WriteLine("Opção invalída!");
+    break;
+}
---
+++ Switchs/Switch06/conversao.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("1 - Celsius para Fahrenheit");
+Console.WriteLine("2 - Fahrenheit para Celsius");
+
+int opcao = int.Parse(Console.ReadLine());
+
+switch (opcao)
+{
+    case 1:
+        Console.Write("Digite a temperatura em Celsius: ");
+        double celsius = double.Parse(Console.ReadLine());
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        Console.WriteLine($"Temperatura em Fahrenheit: {fahrenheit}°F");
+        break;
+
+    case 2:
+        Console.Write("Digite a temperatura em Fahrenheit: ");
+        double fahrenheit2 = double.Parse(Console.ReadLine());
+
+        double celsius2 = (fahrenheit2 - 32) * 5 / 9;
+
+        Console.WriteLine($"Temperatura em Celsius: {celsius2}°C");
+        break;
+
+    default:
+        Console.WriteLine("Opção inválida!");
+        break;
+} 
