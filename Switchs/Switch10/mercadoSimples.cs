7a69ed376a9a21c1e653058ee1e5794006fe9786
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-30 14:38:37 -0300

    ela não te ama

---
+++ Switchs/Switch10/mercadoSimples.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("1 - Comprar");
+Console.WriteLine("2 - Consultar saldo");
+Console.WriteLine("3 - Sair");
+int opcao = int.Parse(Console.ReadLine());
+
+double saldo = 100;
+
+
+switch (opcao)
+{
+    case 1:
+        Console.WriteLine("Digite o valor da compra: ");
+        double compra = double.Parse(Console.ReadLine());
+
+        if (saldo < compra)
+        {
+            Console.WriteLine("Pouco saldo!");
+        }
+        else
+        {
+            saldo = saldo - compra;
+            Console.WriteLine($"Compra efetuada, valor restante do saldo: {saldo}");
+        }
+        break;
+
+    case 2:
+
+        Console.WriteLine($"Seu saldo é de: {saldo}");
+        break;
+
+    case 3:
+        Console.WriteLine("Saindo do sistema...");
+        break;
+}
