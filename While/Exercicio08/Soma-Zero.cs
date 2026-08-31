d08bd263238e832adc4d14c9cc08187e42ea0b53
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-31 14:51:42 -0300

    Soma = zero

---
+++ While/Exercicio08/Soma-Zero.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int soma = 0;
+
+while (numero != 0)
+{
+    soma += numero;
+
+    System.Console.WriteLine("Digite um número: ");
+    numero = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine(soma);
