49e1ecc6cd769b8626375434cf32779d2bf0549f
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-31 15:51:33 -0300

    past

---
+++ While/Exercicio12/numeroSecreto.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Digite o número secreto: ");
+int numero = int.Parse(Console.ReadLine());
+
+while (numero != 7)
+{
+    if (numero > 7)
+    {
+        System.Console.WriteLine("Chutou acima do número secreto!");
+    }
+    else
+    System.Console.WriteLine("Chutou abaixo do número secreto!");
+
+    System.Console.WriteLine("tente novamente: ");
+    numero = int.Parse(Console.ReadLine());
+}
+
+System.Console.WriteLine("Voce acertou o número secreto: " + numero);
