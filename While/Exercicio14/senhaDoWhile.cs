ec052f708acd4e988d456cb146c536d9136ee4b1
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-31 17:15:07 -0300

    doWhille

---
+++ While/Exercicio14/senhaDoWhile.cs
@@ -0,0 +1,10 @@
+﻿string senha;
+
+do
+{
+    Console.WriteLine("Digite sua senha: ");
+    senha = Console.ReadLine();
+
+} while (senha != "1234");
+
+Console.WriteLine("Acesso permitido!");
---
+++ While/Exercicio15/menu.cs
@@ -0,0 +1,31 @@
+﻿int opcao;
+
+do
+{
+    System.Console.WriteLine("===== MENU =====");
+    System.Console.WriteLine("1 - Dizer Olá");
+    System.Console.WriteLine("2 - Mostrar número");
+    System.Console.WriteLine("3 - Dizer Tchau");
+    System.Console.WriteLine("4 - Sair");
+    opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            Console.WriteLine("Olá");
+            break;
+
+        case 2:
+            Console.WriteLine("18");
+            break;
+
+        case 3:
+            Console.WriteLine("Tchau!");
+            break;
+
+        case 4:
+            Console.WriteLine("Saindo!");
+            break;
+    }
+} while (opcao != 4);
+Console.WriteLine("Finalizado!");
