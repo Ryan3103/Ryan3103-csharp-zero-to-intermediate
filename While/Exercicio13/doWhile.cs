4602c1d08faf90d03027ad274c935eb14f05bcc4
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-31 16:59:18 -0300

    dowhile

---
+++ While/Exercicio13/doWhile.cs
@@ -0,0 +1,11 @@
+﻿int opcao;
+
+do
+{
+    System.Console.WriteLine("1 - Olá");
+    System.Console.WriteLine("2 - Tchau");
+    System.Console.WriteLine("3 - Sair");
+
+    opcao = int.Parse(Console.ReadLine());
+
+} while (opcao != 3);
