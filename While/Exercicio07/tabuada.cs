ce60e2755a0f4ffcf998fc02e4daf07e1cc551f3
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-31 14:37:29 -0300

    tabuada

---
+++ While/Exercicio07/tabuada.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int multiplicador = 1;
+
+while (multiplicador <= 10)
+{
+    System.Console.WriteLine($"A multiplicação de {numero} x {multiplicador} é: {numero * multiplicador}");
+    multiplicador++;
+}
