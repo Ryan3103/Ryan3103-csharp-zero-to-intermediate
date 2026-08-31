553159db87cde84c742e77d97daba706e35aa07c
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-31 15:03:52 -0300

    positivo

---
+++ While/Exercicio09/positivo.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Digite números postivos e negativos: ");
+int numero = int.Parse(Console.ReadLine());
+
+int positivos = 0;
+
+while (numero != 0)
+{
+    if (numero > 0)
+    {
+        positivos++;
+    }
+     System.Console.WriteLine("Digite outro numero: ");
+     numero = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"Quantidade de positivos: {positivos}");
