ddfcb5c4f53f7bb56d88f1779c6ce904ca443d72
Ryan <ryangabrielsilvag052@gmail.com>
2026-08-31 15:25:55 -0300

    new programs

---
+++ While/Exercicio10/maiorNum.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int maior = numero;
+
+while (numero != 0)
+{
+    System.Console.WriteLine("Digite um número: ");
+    numero = int.Parse(Console.ReadLine());
+
+    if (numero > maior)
+    {
+        maior = numero;
+    }
+}
+System.Console.WriteLine(maior);
---
+++ While/Exercicio11/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Digite suas notas: ");
+double notas = double.Parse(Console.ReadLine());
+
+int quantidade = 0;
+double soma = 0;
+double media;
+
+while (notas != -1)
+{   
+    soma += notas;
+    quantidade++;
+    Console.WriteLine("Digite suas notas: ");
+    notas = double.Parse(Console.ReadLine());
+}
+
+media = soma / quantidade;
+System.Console.WriteLine(media);
