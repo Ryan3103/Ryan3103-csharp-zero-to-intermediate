80aab940ce24810d7599e5ed75bc04da81f22d71
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-01 15:55:53 -0300

    maiorMenor

---
+++ While/Exercicio22/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int maior = 0;
+int menor = numero;
+
+while (numero != 0)
+{
+    if (numero > maior)
+    {
+        maior = numero;
+    }
+    else if (numero < menor)
+    {
+        menor = numero;
+    }
+
+    System.Console.WriteLine("Digite um número: ");
+    numero = int.Parse(Console.ReadLine());
+}
+
+System.Console.WriteLine($"Maior: {maior}");
+System.Console.WriteLine($"Menor: {menor}");
