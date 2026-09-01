cfe62e08475e1d1c08a75107ea899274683c7afa
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-01 15:18:00 -0300

    Media dos positivos

---
+++ While/Exercicio21/mediaPositivos.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int positivo = 0;
+int soma = 0;
+
+while (numero != 0)
+{
+    if (numero > 0)
+    {
+        positivo++;
+        soma += numero;
+    }
+
+    System.Console.WriteLine("Digite um número: ");
+    numero = int.Parse(Console.ReadLine());
+}
+double media = soma / positivo;
+
+System.Console.WriteLine($"A soma dos números positivos é {soma}");
+System.Console.WriteLine($"A média dos números positivos é {media}");
