42e3fd460ee8264345b4d254dfa3a6a7c11ad863
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-01 15:18:52 -0300

    menor

---
+++ While/Exercicio18/menor.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int menor = numero;
+
+while (numero != 0)
+{
+    if (numero < menor)
+    {
+        menor = numero;
+    }
+    System.Console.WriteLine("Digite um número: ");
+    numero = int.Parse(Console.ReadLine());
+}
+
+System.Console.WriteLine($"O menor número é {menor}");
