9366df43d1410186ad2be641ae8cfd95e9837549
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-01 14:28:38 -0300

    Maior

---
+++ While/Exercicio17/maior.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Digite um numero: ");
+int numero = int.Parse(Console.ReadLine());
+
+int maior = 0;
+
+while (numero != 0)
+{
+    if (numero > maior)
+    {
+        maior = numero;
+    }
+
+    System.Console.WriteLine("Digite um numero é: ");
+    numero = int.Parse(Console.ReadLine());
+}
+
+System.Console.WriteLine($"Maior número {maior}!");
