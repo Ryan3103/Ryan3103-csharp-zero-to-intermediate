8e956e19f0a447dfe67030acfad45870277b7147
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-01 14:57:33 -0300

    Soma de positivos

---
+++ While/Exercicio20/somaPosit.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int positivos = 0;
+int soma = 0;
+
+while (numero != 0)
+{
+    if (numero > 0)
+    {
+        positivos++;
+        soma += numero;
+    }
+    System.Console.WriteLine("Digite um número: ");
+    numero = int.Parse(Console.ReadLine());
+}
+
+System.Console.WriteLine($"A soma dos {positivos} positivos é {soma}.");
