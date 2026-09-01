184f79d7c9702a068bb26de33456242d109d8ca7
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-01 14:16:57 -0300

    positiv

---
+++ While/Exercicio16/posNegZer.cs
@@ -0,0 +1,24 @@
+﻿System.Console.WriteLine("Digite um numero: ");
+int numero = int.Parse(Console.ReadLine());
+
+int positivos = 0;
+int negativos = 0;
+int zero = 0;
+
+while (numero != 0)
+{   
+    if(numero > 0)
+    {
+        positivos++;
+    }
+    else if (numero < 0)
+    {
+        negativos++;
+    }
+    System.Console.WriteLine("Digite um numero: ");
+    numero = int.Parse(Console.ReadLine());
+}
+
+System.Console.WriteLine($"Digitou {positivos} positivos");
+System.Console.WriteLine($"Digitou {negativos} negativos");
+System.Console.WriteLine($"Digitou {zero} zeros");
