c69752003347f771ea4ba1c3ea8a5bcc6342b2e6
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-01 14:48:43 -0300

    positivos e negativos

---
+++ While/Exercicio19/positivoNegativo.cs
@@ -0,0 +1,22 @@
+﻿System.Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int positivos = 0;
+int negativos = 0;
+
+while (numero != 0)
+{
+    if(numero > 0)
+    {
+        positivos++;
+    }
+    else if (numero < 0)
+    {
+        negativos++;
+    }
+    System.Console.WriteLine("Digite um numero: ");
+    numero = int.Parse(Console.ReadLine());
+}
+
+System.Console.WriteLine($"Digitou {positivos} positivos.");
+System.Console.WriteLine($"Digitou {negativos} negativos.");
