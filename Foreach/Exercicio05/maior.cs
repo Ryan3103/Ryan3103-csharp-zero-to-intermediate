35664884fe37d224559e151a91d5b053867b265c
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-04 18:54:24 -0300

    maior

---
+++ Foreach/Exercicio05/maior.cs
@@ -0,0 +1,12 @@
+﻿int [] numeros = {12, 5, 28, 9, 34, 17 };
+
+int maior = 1;
+
+foreach (int numero in numeros)
+{
+    if (numero > maior)
+    {
+        maior = numero;
+    }
+}
+Console.WriteLine($"O maior número é {maior}");
