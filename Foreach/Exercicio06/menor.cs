af0af7abe44faa0352ff01aef7b834510697a4d9
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-04 19:20:43 -0300

    menor

---
+++ Foreach/Exercicio06/menor.cs
@@ -0,0 +1,13 @@
+﻿int [] numeros = {18, 7, 25, 3, 14, 9};
+
+int menor = numeros[0];
+
+foreach (int numero in numeros)
+{
+    if(numero < menor)
+    {
+        menor = numero;
+    }
+}
+
+Console.WriteLine($"O menor número é: {menor}");
