8a2f233a9afb333a385905043bd03941bcb1110a
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-04 15:31:11 -0300

    Maior menor e soma

---
+++ For/Exercicio10/maiorMenor.cs
@@ -0,0 +1,24 @@
+﻿int soma = 0;
+int maior = 1;
+int menor = 1;
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine("Digite um número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    soma += numero;
+
+    if (numero > maior)
+    {
+        maior = numero;
+    }
+    if (numero < menor)
+    {
+        menor = numero;
+    }
+}
+
+Console.WriteLine($"Maior: {maior}");
+Console.WriteLine($"Menor: {menor}");
+Console.WriteLine($"Soma: {soma}");
