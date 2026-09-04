68d03c366cb36db4ba32bd04ffdb77eddcf7be58
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-04 14:26:24 -0300

    tabuada usando for

---
+++ For/Exercicio07/tabuada.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+for (int i = 1;i <= 10; i++)
+{
+    Console.WriteLine($"A multiplicação de {i} x {numero} é {i * numero}");
+}
