0996f3c012a3d40fa784397eb9e2d1b7f3ed7c8b
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-04 14:46:10 -0300

    Numeros pares ate numero escolhido

---
+++ For/Exercicio08/paresNum.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= numero; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
