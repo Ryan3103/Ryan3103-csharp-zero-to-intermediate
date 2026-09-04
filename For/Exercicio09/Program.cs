adf47594c4d53b2b5eacdc8d2b10764f1f0d8ede
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-04 15:08:14 -0300

    Soma dos numeros for

---
+++ For/Exercicio09/Program.cs
@@ -0,0 +1,11 @@
+﻿int soma = 0;
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine("Digite um número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    soma+=numero;
+}
+
+Console.WriteLine($"A soma dos números é: {soma}");
