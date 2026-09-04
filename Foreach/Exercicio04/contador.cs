036410fcbef5f89f7b52026900cb97c098187a41
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-04 18:44:46 -0300

    quantidade de pares

---
+++ Foreach/Exercicio04/contador.cs
@@ -0,0 +1,12 @@
+﻿int [] numeros = {4, 7, 10, 13, 16, 21, 24};
+
+int contador = 0;
+
+foreach (int numero in numeros)
+{
+    if (numero % 2 == 0){
+        contador++;
+    }
+}
+
+Console.WriteLine($"A quantidade de números pares é: {contador}");
