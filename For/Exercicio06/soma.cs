d698b3a8d2547ea6fa380313d3758ef16b431646
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-04 14:03:18 -0300

    Soma For

---
+++ For/Exercicio06/soma.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int soma = 0;
+
+for(int i = 1; i <= numero; i++)
+{
+    soma += i;
+}
+
+Console.WriteLine($"Soma: {soma}");
