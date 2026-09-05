5ba01b7b434b1d31b2582189c8674a3cf1df9e65
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-05 19:33:09 -0300

    Media

---
+++ Foreach/Exercicio08/media.cs
@@ -0,0 +1,12 @@
+﻿int[] numeros = { 8, 12, 20, 16, 24 };
+
+int soma = 0;
+
+foreach (int numero in numeros)
+{
+    soma += numero;
+}
+
+int media = soma / numeros.Length;
+
+Console.WriteLine($"A média é: {media}");
