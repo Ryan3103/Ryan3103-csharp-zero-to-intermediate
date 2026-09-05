6877d89a35dea2ebc29444c4b5b74894643839e7
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-05 19:55:21 -0300

    Maior - Menor numero

---
+++ Foreach/Exercicio09/maiorMenor.cs
@@ -0,0 +1,19 @@
+﻿int [] numeros = {15, 4, 27, 9, 31, 12, 6};
+
+int maior = numeros[0];
+int menor = numeros[0];
+
+foreach (int numero in numeros)
+{
+    if (numero > maior)
+    {
+        maior = numero;
+    }
+    if (numero < menor)
+    {
+        menor = numero;
+    }
+}
+
+Console.WriteLine($"O maior número é: {maior}");
+Console.WriteLine($"O menor número é: {menor}");
