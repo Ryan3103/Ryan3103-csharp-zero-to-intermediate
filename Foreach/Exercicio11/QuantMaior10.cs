7ffe2bdb1f5e7bbabe6205062f52f438ac0be964
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-05 20:49:33 -0300

    Quantidade maior que 10

---
+++ Foreach/Exercicio11/QuantMaior10.cs
@@ -0,0 +1,13 @@
+﻿int[] numeros = { 5, 12, 18, 3, 27, 8, 21, 30 };
+
+int contador = 0;
+
+foreach (int numero in numeros)
+{
+    if (numero > 10)
+    {
+        contador++;
+    }
+}
+
+Console.WriteLine($"Quantidade de números maiores de 10: {contador}");
