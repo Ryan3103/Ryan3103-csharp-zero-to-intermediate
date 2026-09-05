fde0e95d8884967f155b3b7f9f6a916b0e3571a9
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-05 19:14:23 -0300

    Quantidade e soma de pares

---
+++ Foreach/Exercicio07/SomaQuant.cs
@@ -0,0 +1,16 @@
+﻿int [] numeros = {4, 9, 12, 7, 20, 15, 8};
+
+int contador = 0;
+int soma = 0;
+
+foreach (int numero in numeros)
+{
+    if (numero % 2 == 0)
+    {
+        contador++;
+        soma += numero;
+    }
+}
+
+Console.WriteLine($"Quantidade de pares: {contador}");
+Console.WriteLine($"A soma dos números pares: {soma}");
