4c89422825e9f3e527abe77187b958f60212ebab
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-05 20:58:19 -0300

    Soma dos maiores de 10

---
+++ Foreach/Exercicio12/SomaMaior10.cs
@@ -0,0 +1,13 @@
+﻿int[] numeros = { 4, 15, 8, 22, 7, 30, 11, 6 };
+
+int soma = 0;
+
+foreach (int numero in numeros)
+{
+    if(numero > 10)
+    {
+        soma += numero;
+    }
+}
+
+Console.WriteLine($"Soma: {soma}");
