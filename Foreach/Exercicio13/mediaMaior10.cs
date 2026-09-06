41f15b7226e23a531cd79a6a6e961c08095210f3
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-05 21:17:06 -0300

    Media maior de 10

---
+++ Foreach/Exercicio13/mediaMaior10.cs
@@ -0,0 +1,18 @@
+﻿int [] numeros = {5, 12, 7, 20, 3, 18, 10, 25};
+
+double soma = 0;
+int contador = 0;
+double media;
+
+foreach (int numero in numeros)
+{
+    if (numero > 10)
+    {
+        contador++;
+        soma += numero;
+    }
+}
+
+media = soma / contador;
+
+Console.WriteLine($"Média: {media}");
