62f004219aa467081011f3a12c0851c687f948f3
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-06 10:24:39 -0300

    Trim

---
+++ Strings/Exercicio02/Trim.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Digite uma palavra com espaço no começo: ");
+string palavraOriginal = Console.ReadLine();
+
+Console.WriteLine($"Original: {palavraOriginal}");
+Console.WriteLine($"Com trim: {palavraOriginal.Trim()}");
