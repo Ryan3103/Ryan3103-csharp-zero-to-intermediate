320c9a8362c43affef6b7e8c040b7394558f40a2
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-06 10:15:05 -0300

    Upper, lower e length

---
+++ Strings/Exercicio01/palavra.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Digite uma palavra: ");
+string palavra = Console.ReadLine();
+
+Console.WriteLine($"Palavra em maiusculo: {palavra.ToUpper()}");
+Console.WriteLine($"Palavra em minusculo: {palavra.ToLower()}");
+Console.WriteLine($"Quantidade de caracteres: {palavra.Length}");
