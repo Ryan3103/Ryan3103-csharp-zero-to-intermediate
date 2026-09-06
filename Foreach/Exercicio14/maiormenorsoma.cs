b6bd7ce5b16a06d1c6c1ce268d53225893885257
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-05 22:07:04 -0300

    Maior, menor, soma e contador

---
+++ Foreach/Exercicio14/maiormenorsoma.cs
@@ -0,0 +1,31 @@
+﻿int [] numeros = {7, 14, 3, 22, 9, 18, 5, 30};
+
+int maior = numeros [0];
+int menor = numeros [0];
+int soma = 0;
+int contador = 0;
+
+foreach (int numero in numeros)
+{
+    if (numero > maior)
+    {
+        maior = numero;
+    }
+    if (numero < menor)
+    {
+        menor = numero;
+    }
+    if (numero % 2 == 0)
+    {
+        soma += numero;
+    }
+    if (numero % 2 != 0)
+    {
+        contador++;
+    }
+}
+
+Console.WriteLine($"Maior: {maior}");
+Console.WriteLine($"Menor: {menor}");
+Console.WriteLine($"Soma: {soma}");
+Console.WriteLine($"Quantidade de números impares: {contador}");
