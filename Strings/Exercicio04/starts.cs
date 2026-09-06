7fdef3793c1e4924567aad6bc83ebb28aaa423e1
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-06 10:50:03 -0300

    StartsWith e EndsWith

---
+++ Strings/Exercicio04/starts.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite uma palavra: ");
+string palavra = Console.ReadLine();
+
+if (palavra.StartsWith("A"))
+{
+    Console.WriteLine("Começa com A");
+}
+if (palavra.EndsWith("o"))
+{
+    Console.WriteLine("Termina com o");
+}
