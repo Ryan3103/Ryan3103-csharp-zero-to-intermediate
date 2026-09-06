61f07f35b119f2bd6027c1c5db9ddd6a0066487c
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-06 10:39:04 -0300

    Contains

---
+++ Strings/Exercicio03/Contains.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Digite uma frase: ");
+string frase = Console.ReadLine();
+
+Console.WriteLine("Digite uma palavra para procurar: ");
+string palavra = Console.ReadLine();
+
+if (frase.Contains(palavra))
+{
+    Console.WriteLine($"A palavra foi encontrada!");
+}
+else 
+{
+Console.WriteLine("A palavra não foi encontrada!");
+}
