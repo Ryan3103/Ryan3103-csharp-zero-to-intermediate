f11208aef4859ea181837f0eed64d4516fe6ef5b
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-07 14:27:40 -0300

    Indice - Primeira/Ultima letra

---
+++ Strings/Exercicio06/indice.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Digite uma palavra: ");
+string palavra = Console.ReadLine();
+
+Console.WriteLine($"Primeira letra: {palavra[0]}");
+Console.WriteLine($"Ultima letra: {palavra[palavra.Length - 1]}");
