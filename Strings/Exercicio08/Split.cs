f8d2516c151c02be0794224d0b90b700583105f0
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-07 14:48:55 -0300

    Split

---
+++ Strings/Exercicio08/Split.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite uma frase: ");
+string palavras = Console.ReadLine();
+
+string [] frase = palavras.Split(" ");
+
+foreach (string palavra in frase)
+{
+    Console.WriteLine(palavra);
+}
