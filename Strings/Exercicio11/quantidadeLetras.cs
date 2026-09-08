c2065b3687ac1c3005aee59e8922fe67564fa277
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-08 14:27:36 -0300

    Quantidade de letras

---
+++ Strings/Exercicio11/quantidadeLetras.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Digite uma palavra: ");
+string palavra = Console.ReadLine();
+
+int contador = 0;
+
+foreach (char letra in palavra)
+{
+    if (letra == 'a')
+    {
+        contador++;
+    }
+}
+
+Console.WriteLine($"A letra 'a' aparece {contador} vezes.");
