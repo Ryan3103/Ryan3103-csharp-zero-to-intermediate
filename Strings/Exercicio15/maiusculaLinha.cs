f3483e4e2082a38df07388c0810cd59d24c12e9d
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-08 15:40:57 -0300

    Maiuscula por linha

---
+++ Strings/Exercicio15/maiusculaLinha.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("DIgite uma frase: ");
+string frase = Console.ReadLine();
+
+frase = frase.ToUpper();
+string [] palavra = frase.Split(" ");
+
+foreach (string tudo in palavra)
+{
+    Console.WriteLine(tudo);
+}
