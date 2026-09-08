61dbd906e0086c497bd3b54998624ad090371b01
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-08 16:04:47 -0300

    desafio

---
+++ Strings/Exercicio16/desafio.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Digite uma frase: ");
+string frase = Console.ReadLine();
+
+Console.WriteLine($"Maiusculo: {frase.ToUpper()}");
+Console.WriteLine($"Letras: {frase.Length}");
+
+string [] palavras = frase.Split(" ");
+Console.WriteLine($"Palavras: {palavras.Length}");
+Console.WriteLine($"Primeira palavra: {palavras[0]}");
+Console.WriteLine($"Ultima palavra: {palavras[palavras.Length -1]}");
