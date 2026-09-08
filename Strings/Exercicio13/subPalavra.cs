22ade785557c51eba9e81938e9d936e72d69b300
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-08 14:56:07 -0300

    substituir palavra

---
+++ Strings/Exercicio13/subPalavra.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Digite uma frase: ");
+string frase = Console.ReadLine();
+
+Console.WriteLine("Digite a palavra que quer substituir: ");
+string palavra = Console.ReadLine();
+
+Console.WriteLine("Digite a  nova palavra: ");
+string novaPalavra = Console.ReadLine();
+
+frase = frase.Replace(palavra, novaPalavra);
+
+Console.WriteLine($"Frase substituida: {frase}");
