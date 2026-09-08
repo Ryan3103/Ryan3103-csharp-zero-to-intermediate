3a5bdf6b9b47f730db44346b741668786b3ca368
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-08 14:05:42 -0300

    Trim, Upper e Split

---
+++ Strings/Exercicio09/Junção.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Digite uma frase: ");
+string frase = Console.ReadLine();
+
+frase = frase.Trim();
+frase = frase.ToUpper();
+
+string [] palavras = frase.Split(" ");
+
+foreach (string palavra in palavras)
+{
+    Console.WriteLine(palavra);
+}
