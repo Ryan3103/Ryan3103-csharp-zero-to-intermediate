2a9d30c69aead92754548f828503c71056499320
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-08 14:16:09 -0300

    Contar palavra

---
+++ Strings/Exercicio10/contarPalavra.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Digite uma frase: ");
+string frase = Console.ReadLine();
+
+string [] palavras = frase.Split(" ");
+Console.WriteLine($"Quantidade de palavras: {palavras.Length}");
