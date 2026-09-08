6efe78a1a7269c2957a8ec4b16a05b4b7841c565
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-08 15:14:23 -0300

    primeiros e ultimos caracteres

---
+++ Strings/Exercicio14/primUlt.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Digite uma palavra: ");
+string palavra = Console.ReadLine();
+
+Console.WriteLine(palavra.Length);
+Console.WriteLine($"3 primeiros caracteres: {palavra.Substring(0, 3)}");
+Console.WriteLine($"3 ultimos caracteres: {palavra.Substring(palavra.Length -3, 3)}");
