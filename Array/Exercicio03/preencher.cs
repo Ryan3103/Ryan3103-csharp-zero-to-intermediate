2d179c17f122f556683c5a99e8b9110044b94879
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-10 15:42:47 -0300

    Preencher

---
+++ Array/Exercicio03/preencher.cs
@@ -0,0 +1,14 @@
+﻿int [] numeros = new int [5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Digite um número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Resultado: ");
+
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
