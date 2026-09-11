36c0aaa43577166398e8312905c71deaa4b7e922
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-11 14:05:46 -0300

    Maior número digitado

---
+++ For/Exercicio16/Program.cs
@@ -0,0 +1,14 @@
+﻿int maior = 0;
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Digite o {i}* número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero > maior)
+    {
+        maior = numero;
+    }
+}
+
+Console.WriteLine($"Maior número: {maior}");
