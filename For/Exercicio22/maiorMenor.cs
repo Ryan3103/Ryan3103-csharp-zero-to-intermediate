87eba5e9f9456782de60c05cd0afe7a44f04eb50
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-11 17:54:58 -0300

    maior menor

---
+++ For/Exercicio22/maiorMenor.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Digite o 1* número: ");
+int menor = int.Parse(Console.ReadLine());
+
+int maior = 1;
+
+for (int i = 2; i <= 10; i++)
+{
+    Console.WriteLine($"Digite o {i}* número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero > maior)
+    {
+        maior = numero;
+    }
+    else if (numero < menor)
+    {
+        menor = numero;
+    }
+}
+
+Console.WriteLine($"Maior número: {maior}");
+Console.WriteLine($"Menor número: {menor}");
