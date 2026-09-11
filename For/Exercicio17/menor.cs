0f492a9f759490146d9a480c5ac38abe6a5cc74f
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-11 14:21:56 -0300

    menor número

---
+++ For/Exercicio17/menor.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Digite o 1* número: ");
+int menor = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= 5; i++)
+{
+    Console.WriteLine($"Digite o {i}* número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero < menor)
+    {
+        menor = numero;
+    }
+}
+
+Console.WriteLine($"Menor número: {menor}");
