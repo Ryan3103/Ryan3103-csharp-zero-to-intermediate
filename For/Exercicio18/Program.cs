283a62d0a204585c9d52826ef9bc2d7884242fa0
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-11 14:35:38 -0300

    Soma e media

---
+++ For/Exercicio18/Program.cs
@@ -0,0 +1,14 @@
+﻿int soma = 0;
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Digite o {i}* número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    soma += numero;
+}
+
+double media = soma / 5;
+
+Console.WriteLine($"Soma: {soma}");
+Console.WriteLine($"Média: {media}");
