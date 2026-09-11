a01813c972465c17af9e62fdc6bbc122b6d286fa
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-11 18:25:28 -0300

    Fatorial

---
+++ For/Exercicio24/Fatorial.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int multiplicacao = 1;
+
+for(int i = 1; i <= numero; i++)
+{
+    multiplicacao *= i;
+}
+
+Console.WriteLine($"!{numero} = {multiplicacao}");
