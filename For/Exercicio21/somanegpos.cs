ddecbbc4763f8be05310f8c584dc88f5de57cff7
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-11 15:38:25 -0300

    Soma positivos e negativos

---
+++ For/Exercicio21/somanegpos.cs
@@ -0,0 +1,20 @@
+﻿int positivos = 0;
+int negativos = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"Digite o {i}* número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero > 0)
+    {
+        positivos += numero;
+    }
+    else if (numero < 0)
+    {
+        negativos += numero;
+    }
+}
+
+Console.WriteLine($"Soma dos positivos: {positivos}");
+Console.WriteLine($"Soma dos negativos: {negativos}");
