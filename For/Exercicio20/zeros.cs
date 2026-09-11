5e56c06e7e680d023d1ad6b72278bc3013fcb659
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-11 15:12:42 -0300

    Positivo,negativo e zero

---
+++ For/Exercicio20/zeros.cs
@@ -0,0 +1,26 @@
+﻿int positivo = 0;
+int negativo = 0;
+int zeros = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"Digite o {i}* número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero > 0)
+    {
+        positivo++;
+    }
+    else if (numero < 0)
+    {
+        negativo++;
+    }
+    else if (numero == 0)
+    {
+        zeros++;
+    }
+}
+
+Console.WriteLine($"Positivos: {positivo}");
+Console.WriteLine($"Negativos: {negativo}");
+Console.WriteLine($"Zeros: {zeros}");
