db93ebfabdfe6ecbf872c05a0e0e86b9ca2cf935
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-11 18:49:46 -0300

    Primos

---
+++ For/Exercicio25/primos.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+bool primo = true;
+
+for (int i = 2; i < numero; i++)
+{
+    if (numero % i == 0)
+    {
+        primo = false;
+    }
+}
+if (primo)
+{
+    Console.WriteLine("Primo");
+}
+else
+{
+    Console.WriteLine("Não primo");
+}
