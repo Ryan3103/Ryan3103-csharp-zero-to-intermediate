869e7ef9faa5e8a2611dda7f70ed50a3234f0197
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-11 14:54:02 -0300

    Positivos e nagativos

---
+++ For/Exercicio19/positivoNegativo.cs
@@ -0,0 +1,20 @@
+﻿int positivo = 0;
+int negativo = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"Digite o {i}* número");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero > 0)
+    {
+        positivo++;
+    }
+    else if (numero < 0)
+    {
+        negativo++;
+    }
+}
+
+Console.WriteLine($"Quantidade de positivos: {positivo}");
+Console.WriteLine($"Quantidade de negativos: {negativo}");
