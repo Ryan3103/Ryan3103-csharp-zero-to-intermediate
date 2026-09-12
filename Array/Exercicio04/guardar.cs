1ed33ddfa4d037179240bfa365089e772cbc877c
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-12 16:31:00 -0300

    guardar

---
+++ Array/Exercicio04/guardar.cs
@@ -0,0 +1,11 @@
+﻿int[] numeros = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Digite o {i + 1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(numeros[i]);
+}
