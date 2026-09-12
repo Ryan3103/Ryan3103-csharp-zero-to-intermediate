43e80aa9780509f065e30374ae19a038cfe3c57b
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-12 19:39:22 -0300

    Somas array

---
+++ Array/Exercicio05/soma.cs
@@ -0,0 +1,16 @@
+﻿int[] numeros = new int[5];
+
+int soma = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Digite o {i + 1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < 5; i++)
+{
+    soma += numeros[i];
+}
+
+Console.WriteLine($"A soma dos números inseridos é {soma}");
