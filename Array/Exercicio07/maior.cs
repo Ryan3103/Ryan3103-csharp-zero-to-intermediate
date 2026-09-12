fd520ac2e50c3a8604d8db5d48dedf2dd40a1f9a
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-12 20:20:27 -0300

    maior número

---
+++ Array/Exercicio07/maior.cs
@@ -0,0 +1,18 @@
+﻿int[] numeros = new int[5];
+
+int maior = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Digite o {i + 1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < 5; i++)
+{
+    if (numeros[i] > maior)
+    {
+        maior = numeros[i];
+    }
+}
+
+Console.WriteLine($"O maior número é: {maior}");
