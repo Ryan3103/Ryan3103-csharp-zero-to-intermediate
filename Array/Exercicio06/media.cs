1cb30da997037bfc9b378b374447aba1743e5f3c
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-12 19:47:58 -0300

    media

---
+++ Array/Exercicio06/media.cs
@@ -0,0 +1,19 @@
+﻿int [] numeros = new int [5];
+
+int soma = 0;
+double media = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Digite o {i + 1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < 5; i++)
+{
+    soma += numeros[i];
+}
+
+media = soma / 5;
+
+Console.WriteLine($"Média: {media}");
