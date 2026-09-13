fec1445943e928c139df3a4c9e70cd369309a6e7
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-13 15:37:22 -0300

    Menor entre digitados

---
+++ Array/Exercicio08/menor.cs
@@ -0,0 +1,19 @@
+﻿int[] numeros = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Digite o {i + 1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+int menor = numeros[0];
+
+for (int i = 0; i < 5; i++)
+{
+    if (numeros[i] < menor)
+    {
+        menor = numeros[i];
+    }
+}
+
+Console.WriteLine($"Menor: {menor}");
