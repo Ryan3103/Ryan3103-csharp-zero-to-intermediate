f2364c0e828dddaae3636676a797810a62bdb57d
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-14 15:39:12 -0300

    Maior número e posição

---
+++ Array/Exercicio15/maiorPosicao.cs
@@ -0,0 +1,22 @@
+﻿int[] numeros = new int[5];
+
+int maior = numeros[0];
+int posicao = -1;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Digite o {i + 1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < 5; i++)
+{
+    if (numeros[i] > maior)
+    {
+        maior = numeros[i];
+        posicao = i;
+    }
+}
+
+Console.WriteLine($"Maior: {maior}");
+Console.WriteLine($"Posição: {posicao}");
