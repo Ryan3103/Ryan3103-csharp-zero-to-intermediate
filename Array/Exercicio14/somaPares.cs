6fb9c12791774193a559b9acdd509910add5f6c8
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-14 15:24:29 -0300

    soma dos pares

---
+++ Array/Exercicio14/somaPares.cs
@@ -0,0 +1,19 @@
+﻿int [] numeros = new int [10];
+
+int soma = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Digite o {i + 1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < 10; i++)
+{
+    if (numeros[i] % 2 == 0)
+    {
+        soma += numeros[i];
+    }
+}
+
+Console.WriteLine($"Soma dos pares: {soma}");
