bafe0d69850752061562a9ce38cf900c896a49ce
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-14 13:49:51 -0300

    Procurar números

---
+++ Array/Exercicio10/procurarNumero.cs
@@ -0,0 +1,29 @@
+﻿int[] numeros = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Digite o {i + 1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Digite o número que quer encontrar: ");
+int busca = int.Parse(Console.ReadLine());
+
+bool encontrar = false;
+
+for (int i = 0; i < 5; i++)
+{
+    if (numeros[i] == busca)
+    {
+        encontrar = true;
+    }
+}
+
+if (encontrar)
+{
+    Console.WriteLine("Número encontrado!");
+}
+else
+{
+    Console.WriteLine("Número não encontrado!");
+}
