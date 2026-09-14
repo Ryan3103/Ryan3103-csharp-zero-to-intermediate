f8c05370a2e6bf48372d79d0bd987a0e6735886c
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-14 13:28:15 -0300

    Contador de par

---
+++ Array/Exercicio09/contadorPar.cs
@@ -0,0 +1,16 @@
+﻿int[] numeros = new int[10];
+
+int contador = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Digite o {i + 1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+
+    if (numeros[i] % 2 == 0)
+    {
+        contador++;
+    }
+}
+
+Console.WriteLine($"Digitou {contador} números pares.");
