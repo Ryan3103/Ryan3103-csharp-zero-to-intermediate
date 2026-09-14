8718b4883d98ac3232b84f2d0bd46f241c6d181d
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-14 16:23:45 -0300

    Contar qual array tem mais números pares

---
+++ Array/Exercicio17/arraynúmeros.cs
@@ -0,0 +1,39 @@
+﻿int[] numeros1 = new int[5];
+int[] numeros2 = new int[5];
+
+int contador1 = 0;
+int contador2 = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Digite o {i + 1}º número do array 1:");
+    numeros1[i] = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Digite o {i + 1}º número do array 2:");
+    numeros2[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < 5; i++)
+{
+    if (numeros1[i] % 2 == 0)
+    {
+        contador1++;
+    }
+    if (numeros2[i] % 2 == 0)
+    {
+        contador2++;
+    }
+}
+
+if (contador1 > contador2)
+{
+    Console.WriteLine($"Array 1 tem mais pares: {contador1}");
+}
+else if (contador1 < contador2)
+{
+    Console.WriteLine($"Array 2 tem mais pares: {contador2}");
+}
+else
+{
+    Console.WriteLine($"Iguais: Array 1 = {contador1}, Array 2 = {contador2}");
+}
