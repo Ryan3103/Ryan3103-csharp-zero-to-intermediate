3687cb104874e3c6856fcdec6b37178bf5193a70
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-14 15:01:10 -0300

    Contar ocorrências

---
+++ Array/Exercicio12/contador.cs
@@ -0,0 +1,22 @@
+﻿int[] numeros = new int[10];
+
+int contador = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Digite o {i + 1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Digite um número para encontra-lo: ");
+int buscar = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < 10; i++)
+{
+    if (numeros[i] == buscar)
+    {
+        contador++;
+    }
+}
+
+Console.WriteLine($"O número aparece {contador} vezes.");
