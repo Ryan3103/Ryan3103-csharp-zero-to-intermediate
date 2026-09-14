13a3e8ed29dc4e83d2ca97525cdcaac3e080fd3c
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-14 14:34:36 -0300

    Posição do número

---
+++ Array/Exercicio11/posicaoNumero.cs
@@ -0,0 +1,31 @@
+﻿int[] numeros = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Digite o {i + 1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Digite o número procurado: ");
+int buscar = int.Parse(Console.ReadLine());
+
+bool encontra = false;
+int posicao = -1;
+
+for (int i = 0; i < 5; i++)
+{
+    if (numeros[i] == buscar)
+    {
+        encontra = true;
+        posicao = i;
+    }
+}
+
+if (encontra)
+{
+    Console.WriteLine($"Encontrado na posição: {posicao}");
+}
+else
+{
+    Console.WriteLine("Número não encontrado: ");
+}
