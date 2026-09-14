4d2f2b2bb418533dc79a19e801c2d9beb3b12ee1
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-14 15:12:45 -0300

    Separar pares e impares

---
+++ Array/Exercicio13/parImpar.cs
@@ -0,0 +1,25 @@
+﻿int [] numeros = new int [10];
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Digite o {i + 1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Números pares: ");
+for (int i = 0; i < 10; i++)
+{
+    if (numeros[i] % 2 == 0)
+    {
+        Console.WriteLine(numeros[i]);
+    }
+}
+
+Console.WriteLine("Números impares: ");
+for (int i = 0; i < 10; i++)
+{
+    if (numeros[i] % 2 != 0)
+    {
+        Console.WriteLine(numeros[i]);
+    }
+}
