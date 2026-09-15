ec181aecadb4a0a19b4e7dcd425ac9115da134a4
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-15 14:00:43 -0300

    Contar positivos, negativos e zeros

---
+++ Array/Exercicio22/contar.cs
@@ -0,0 +1,28 @@
+﻿int[] numeros = new int[10];
+
+int positivo = 0;
+int negativo = 0;
+int zero = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Digite o {i + 1}º número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+
+    if (numeros[i] > 0)
+    {
+        positivo++;
+    }
+    else if (numeros[i] < 0)
+    {
+        negativo++;
+    }
+    else
+    {
+        zero++;
+    }
+}
+
+Console.WriteLine($"Positivos: {positivo}");
+Console.WriteLine($"Negativos: {negativo}");
+Console.WriteLine($"Zeros: {zero}");
