953788af91a42c20e80d0c606ebb887368891a45
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-15 13:00:16 -0300

    Criar um segundo array a partir do primeiro

---
+++ Array/Exercicio18/dobro.cs
@@ -0,0 +1,15 @@
+﻿int [] numeros = new int [5];
+int [] dobro = new int [5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Digite o {i + 1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+    dobro[i] = numeros[i] * 2;
+}
+
+Console.WriteLine("O dobro dos números é: ");
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(dobro[i]);
+}
