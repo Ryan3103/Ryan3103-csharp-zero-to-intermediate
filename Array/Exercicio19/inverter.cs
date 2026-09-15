efbd0488f1680f520878026c450cbe1b11296339
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-15 13:12:45 -0300

    Inverter um array

---
+++ Array/Exercicio19/inverter.cs
@@ -0,0 +1,20 @@
+﻿int [] numeros = new int [5];
+int [] invertido = new int [5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Digite o { i + 1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < 5; i++)
+{
+    invertido[i] = numeros[4 - i];
+}
+
+Console.WriteLine("Array invertido:");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(invertido[i]);
+}
