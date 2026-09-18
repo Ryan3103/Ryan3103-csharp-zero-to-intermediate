4b3322077c2d56031d5f9ca5b7d64aeb904de6f3
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-18 14:21:11 -0300

    Diferentes execeções

---
+++ try-catch/Exercicio03/diferentesExcecoes.cs
@@ -0,0 +1,21 @@
+﻿try
+{
+    Console.WriteLine("Digite o primeiro número: ");
+    int numero1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo número: ");
+    int numero2 = int.Parse(Console.ReadLine());
+
+    int divisao = numero1 / numero2;
+
+    Console.WriteLine($"Resultado: {divisao}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Número inválido!");
+}
+
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Não é possivel dividir por 0");
+}
