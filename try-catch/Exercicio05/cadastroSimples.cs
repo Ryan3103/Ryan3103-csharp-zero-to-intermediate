de2a5acb0c864380080df49f1cbefaf5868c4d43
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-18 14:55:45 -0300

    Cadastro simples

---
+++ try-catch/Exercicio05/cadastroSimples.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Digite seu nome: ");
+string nome = Console.ReadLine();
+
+try
+{
+    Console.WriteLine("Digite sua idade: ");
+    int idade = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite sua altura: ");
+    double altura = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Cadastro: ");
+    Console.WriteLine(nome);
+    Console.WriteLine(idade);
+    Console.WriteLine(altura);
+}
+catch (FormatException)
+{
+    Console.WriteLine("Dados numéricos inválidos!");
+}
