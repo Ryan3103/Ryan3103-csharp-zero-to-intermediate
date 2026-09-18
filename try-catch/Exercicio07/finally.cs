3ae0b85f5d4d3613d2a58920ea607c834892fa01
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-18 15:36:32 -0300

    finally

---
+++ try-catch/Exercicio07/finally.cs
@@ -0,0 +1,15 @@
+﻿try
+{
+    Console.WriteLine("Digite um número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Número digitado: {numero}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Número inválido!");
+}
+finally
+{
+    Console.WriteLine("Programa finalizado!");
+}
