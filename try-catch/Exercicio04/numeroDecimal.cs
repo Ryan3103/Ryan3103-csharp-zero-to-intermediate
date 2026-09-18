e3036bb84fb32d1d529b8d55e8843852aa56bc41
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-18 14:38:41 -0300

    Entradas decimais

---
+++ try-catch/Exercicio04/numeroDecimal.cs
@@ -0,0 +1,11 @@
+﻿try
+{
+    Console.WriteLine("Digite o preço do produto: ");
+    double preco = double.Parse(Console.ReadLine());
+
+    Console.WriteLine(preco);
+}
+catch (FormatException)
+{
+    Console.WriteLine("Preço inválido!");
+}
