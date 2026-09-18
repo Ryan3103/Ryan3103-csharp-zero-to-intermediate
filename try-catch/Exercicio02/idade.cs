b4a6e35d87c529e707b3bd0fb35b8da70a91fa25
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-18 14:02:37 -0300

    Validação idade

---
+++ try-catch/Exercicio02/idade.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Digite uma idade: ");
+
+try
+{
+    int idade = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Sua idade é: {idade}");
+}
+
+catch (FormatException)
+{
+    Console.WriteLine("Idade inválida!");
+}
