635262a3b68798860adf59302d7e60a4efff28c3
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-18 13:56:08 -0300

    Primeiro contato com try/catch

---
+++ try-catch/Exercicio01/trycatch.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Digite um número: ");
+
+try
+{
+    int numero = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Você digitou {numero}");
+}
+catch
+{
+    Console.WriteLine("Digite apenas números");
+}
