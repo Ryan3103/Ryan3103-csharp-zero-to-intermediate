88529f4af57d031b9f30842b10ccabb3fdfcacc7
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-18 17:42:14 -0300

    Exception

---
+++ try-catch/Exercicio09/genérica.cs
@@ -0,0 +1,25 @@
+﻿try
+{
+    Console.WriteLine("Digite o 1º número: ");
+    int n1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o 2º número: ");
+    int n2 = int.Parse(Console.ReadLine());
+
+    double divisao = (double)n1 / n2;
+
+    Console.WriteLine($"Resultado: {divisao}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Entrada inválida!");
+}
+catch (Exception)
+{
+    Console.WriteLine("Outra exceção");
+}
+
+finally
+{
+    Console.WriteLine("Programa finalizado!");
+}
