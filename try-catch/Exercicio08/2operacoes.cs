69b3532592bdc658e5c09cb58e060e5c43aa4bd3
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-18 15:59:06 -0300

    Duas operações protegidas

---
+++ try-catch/Exercicio08/2operacoes.cs
@@ -0,0 +1,25 @@
+﻿try
+{
+    Console.WriteLine("Digite o 1º número: ");
+    int numero1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o 2º número: ");
+    int numero2 = int.Parse(Console.ReadLine());
+
+    double divisao = (double)numero1 / numero2;
+
+    Console.WriteLine($"Resultado: {divisao}");
+}
+
+catch (FormatException)
+{
+    Console.WriteLine("Número inválido!");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Não é possível dividir por zero!");
+}
+finally
+{
+    Console.WriteLine("Programa finalizado!");
+}
