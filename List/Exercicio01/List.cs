7ba9c379adf78a75d6b040f7f8a030841410bfaf
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-21 17:49:52 -0300

    List<string>

---
+++ List/Exercicio01/List.cs
@@ -0,0 +1,14 @@
+﻿List<string> nomes = new List<string>();
+
+nomes.Add("Ryan");
+nomes.Add("Ana");
+nomes.Add("Pedro");
+nomes.Add("Ethan");
+nomes.Add("Junio");
+
+foreach (string nome in nomes)
+{
+    Console.WriteLine(nome);
+}
+
+Console.WriteLine($"Quantidade: {nomes.Count}");
