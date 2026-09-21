2aa873e12db149180b1706de3f352a639cd54714
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-21 16:09:34 -0300

    Validação com throw

---
+++ try-catch/Exercicio10/throw.cs
@@ -0,0 +1,25 @@
+﻿try
+{
+    Console.WriteLine("Digite sua idade: ");
+
+    int idade = int.Parse(Console.ReadLine());
+
+    if (idade < 0)
+    {
+        throw new Exception("A idade não pode ser negativa!");
+    }
+
+    Console.WriteLine($"Sua idade é: {idade}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Digite uma idade válida!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Programa finalizado!");
+}
