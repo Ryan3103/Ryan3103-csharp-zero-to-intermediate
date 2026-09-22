5d0675bdd3e0b7b82ff5fe3ca4d10602420d384a
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-22 13:55:21 -0300

    list int

---
+++ List/Exercicio02/listINT.cs
@@ -0,0 +1,17 @@
+﻿List<int> numeros = new List<int>();
+
+int soma = 0;
+
+numeros.Add(10);
+numeros.Add(20);
+numeros.Add(30);
+numeros.Add(40);
+numeros.Add(50);
+
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+    soma += numero;
+}
+
+Console.WriteLine($"Soma: {soma}");
