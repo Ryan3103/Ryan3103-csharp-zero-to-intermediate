735248736e4991e7424426d1d07a2835d7a74506
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-22 15:01:34 -0300

    Acessando posições da List

---
+++ List/Exercicio04/Acessando.cs
@@ -0,0 +1,11 @@
+﻿List<string> nomes = new List<string>();
+
+nomes.Add("Ryan");
+nomes.Add("Pedro");
+nomes.Add("Gabriel");
+nomes.Add("Junio");
+nomes.Add("Silva");
+
+Console.WriteLine($"Primeiro: {nomes[0]}");
+Console.WriteLine($"Terceiro: {nomes[2]}");
+Console.WriteLine($"Ultimo: {nomes[nomes.Count - 1]}");
