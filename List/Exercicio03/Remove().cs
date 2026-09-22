9981110bf075d38369249f40f2feb6919c882d46
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-22 14:33:36 -0300

    Remove()

---
+++ List/Exercicio03/Remove().cs
@@ -0,0 +1,24 @@
+﻿List<string> nomes = new List<string>();
+
+nomes.Add("Ryan");
+nomes.Add("Gabriel");
+nomes.Add("Silva");
+nomes.Add("Breno");
+nomes.Add("Leal");
+
+Console.WriteLine("Antes: ");
+foreach (string nome in nomes)
+{
+    Console.WriteLine(nome);
+}
+
+nomes.Remove("Breno");
+
+Console.WriteLine();
+Console.WriteLine("Depois: ");
+foreach (string nome in nomes)
+{
+    Console.WriteLine(nome);
+}
+Console.WriteLine();
+Console.WriteLine($"Quantidade: {nomes.Count}");
