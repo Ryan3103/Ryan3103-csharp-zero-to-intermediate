f962b2b4558844b99f65eff86545fbad67ce37ff
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-24 14:46:34 -0300

    Alterando um elemento

---
+++ List/Exercicio08/Alterando.cs
@@ -0,0 +1,14 @@
+﻿List<string>nomes = new List<string>();
+
+nomes.Add("Ryan");
+nomes.Add("Pedro");
+nomes.Add("Gustavo");
+nomes.Add("Joaquim");
+nomes.Add("Bruno");
+
+nomes[3] = "Lucas";
+
+foreach (string nome in nomes)
+{
+    Console.WriteLine(nome);
+}
