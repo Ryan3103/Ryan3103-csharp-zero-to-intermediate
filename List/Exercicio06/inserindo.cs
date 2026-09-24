d61c77edf4907e5a035919ca89c1c2f6bcda8e04
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-24 14:31:57 -0300

    Inserindo em uma posição

---
+++ List/Exercicio06/inserindo.cs
@@ -0,0 +1,13 @@
+﻿List<string> nomes = new List<string>();
+
+nomes.Add("Ryan");
+nomes.Add("Pedro");
+nomes.Add("Matheus");
+nomes.Add("Jacó");
+
+nomes.Insert(1, "Carlos");
+
+foreach (string nome in nomes)
+{
+    Console.WriteLine(nome);
+}
