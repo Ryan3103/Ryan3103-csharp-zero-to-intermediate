d8f6cde6c9815cd1a0c187ec97294e4b3841ec21
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-24 15:55:22 -0300

    Lista de números

---
+++ List/Exercicio12/listanumeros.cs
@@ -0,0 +1,31 @@
+﻿int maior = 0;
+int soma = 0;
+
+List<int> numeros = new List<int>();
+
+numeros.Add(10);
+numeros.Add(20);
+numeros.Add(30);
+numeros.Add(40);
+numeros.Add(50);
+numeros.Add(60);
+numeros.Add(70);
+numeros.Add(80);
+numeros.Add(90);
+numeros.Add(100);
+
+foreach (int numero in numeros)
+{
+    soma += numero;
+
+    if (numero > maior)
+    {
+        maior = numero;
+    }
+
+    Console.WriteLine(numero);
+}
+
+Console.WriteLine($"Quantidade: {numeros.Count}");
+Console.WriteLine($"Maior: {maior}");
+Console.WriteLine($"Soma: {soma}");
