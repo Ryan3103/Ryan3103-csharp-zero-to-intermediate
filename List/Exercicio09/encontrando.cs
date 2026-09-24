a0ebba503b3748775cc75b2ecb879d693599d6c3
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-24 14:57:57 -0300

    Encontrando a posição

---
+++ List/Exercicio09/encontrando.cs
@@ -0,0 +1,10 @@
+﻿List<string> nomes = new List<string>();
+
+nomes.Add("Ryan");
+nomes.Add("Pedro");
+nomes.Add("Joaquim");
+nomes.Add("Walace");
+nomes.Add("Junio");
+
+int posicao = nomes.IndexOf("Joaquim");
+Console.WriteLine($"Posição: {posicao}");
