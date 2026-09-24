cc96453c607ed868b971294c266d556ee751721f
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-24 15:32:25 -0300

    Cadastro de produtos

---
+++ List/Exercicio11/Cadastro.cs
@@ -0,0 +1,21 @@
+﻿List<string> produtos = new List<string>();
+
+produtos.Add("Mouse");
+produtos.Add("Teclado");
+produtos.Add("Monitor");
+produtos.Add("Headset");
+produtos.Add("Fone");
+
+if (produtos.Contains("Mouse"))
+{
+    int posicao = produtos.IndexOf("Mouse");
+    Console.WriteLine($"Posição: {posicao}");
+
+    produtos.Remove("Mouse");
+}
+
+Console.WriteLine();
+foreach (string produto in produtos)
+{
+    Console.WriteLine(produto);
+}
