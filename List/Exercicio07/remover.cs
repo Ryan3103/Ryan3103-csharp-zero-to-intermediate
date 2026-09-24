c85dd9b3b50cf68ce5d4ecbd9902e63f059c0f2f
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-24 14:39:04 -0300

    Remover pela posição

---
+++ List/Exercicio07/remover.cs
@@ -0,0 +1,14 @@
+﻿List<string> nomes = new List<string>();
+
+nomes.Add("Ryan");
+nomes.Add("Pedro");
+nomes.Add("Joaquim");
+nomes.Add("Walace");
+nomes.Add("Junio");
+
+nomes.RemoveAt(2);
+
+foreach (string nome in nomes)
+{
+    Console.WriteLine(nome);
+}
