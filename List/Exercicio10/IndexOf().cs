c3ab1de0df3c6845af68987108cf55e402983ea1
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-24 15:09:51 -0300

    IndexOf()

---
+++ List/Exercicio10/IndexOf().cs
@@ -0,0 +1,16 @@
+﻿List<string> nomes = new List<string>();
+
+nomes.Add("Ryan");
+nomes.Add("Jesus");
+nomes.Add("Breno");
+nomes.Add("João");
+nomes.Add("Matheus");
+
+int posicao = nomes.IndexOf("Jesus");
+
+if (posicao != -1)
+{
+    Console.WriteLine($"Jesus está na posicão {posicao}.");
+}
+else
+    Console.WriteLine("Jesus não está na lista");
