06f22b3a2ebbb33167b0a51a53cb725265b6f874
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-24 14:23:58 -0300

    Verificando se existe um elemento

---
+++ List/Exercicio05/verificando.cs
@@ -0,0 +1,16 @@
+﻿List<string> nomes = new List<string>();
+
+nomes.Add("Ryan");
+nomes.Add("Pedro");
+nomes.Add("Gabriel");
+nomes.Add("Ana");
+nomes.Add("Dimas");
+
+if (nomes.Contains("Ryan"))
+{
+    Console.WriteLine("Ryan está na lista!");
+}
+else
+{
+    Console.WriteLine("Ryan não está na lista!");
+}
