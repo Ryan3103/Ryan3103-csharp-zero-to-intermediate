a5443929536d6187f5a2a41b0d025507da8f31ee
Ryan <ryangabrielsilvag052@gmail.com>
2026-09-26 11:31:03 -0300

    Dictionary

---
+++ Dictionary/Exercicio01/Dictionary.cs
@@ -0,0 +1,19 @@
+﻿Dictionary<string, double> produto = new Dictionary<string, double>();
+
+produto.Add("Caderno", 20);
+produto.Add("Mochila", 100);
+produto.Add("Caneta", 2);
+produto.Add("Borracha", 1.90);
+produto.Add("Corretivo", 2.99);
+
+Console.WriteLine("Digite um nome de produto: ");
+string nome = Console.ReadLine();
+
+if (produto.ContainsKey(nome))
+{
+    Console.WriteLine($"Produto encontrado. Preço: R$:{produto[nome]:F2}");
+}
+else
+{
+    Console.WriteLine("Produto não encontrado.");
+}
